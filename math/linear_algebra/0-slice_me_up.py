5a67081a8afba03cbfb8b18db39f87a1b741f33f
hashthat <samalberts73@gmail.com>
2024-01-28 17:13:08 -0600

    config_file-for-testing!

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
-arr1 = [0] [1]
-arr2 = [0 .. 4]
+arr1 = [0][1]
+arr2 = [3 .. 8]
 arr3 = [1 .. 5]
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
