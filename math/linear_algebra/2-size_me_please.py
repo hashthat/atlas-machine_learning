9ceb0632fb4b6f90dae104ad066c9e87c882b834
hashthat <samalberts73@gmail.com>
2024-01-28 22:14:09 -0600

    pycodestyle

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
+"""
+function to determine if there is a matrix
+the function will express the shape
+"""
+
 
 def matrix_shape(matrix):
-    
-    # check if the matrix is a list or integer
+    """
+    matrix_shape(matrix) distinguishes an integer or list
+    in which the matrix is printed in
+    """
     if isinstance(matrix, list):
-    # is the number a list or an integer?? I need some Recursive help!!!
-    # find the rows and columns of the matrix for the expression needed to solve the equation.
-        
         rows = len(matrix)
         # cols = len(matrix[0]) if matrix and matrix[0] else 0
         shape = []
-        
-        
+
         shape.append(rows)
         if isinstance(matrix[0], list):
             shape.append(matrix_shape(matrix[0])[0])
