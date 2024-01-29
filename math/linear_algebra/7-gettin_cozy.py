b216d2427828e3ab40867d52adf36064116c750a
hashthat <samalberts73@gmail.com>
2024-01-29 01:01:00 -0600

    working

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -9,3 +9,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
     assume all elements with same dimension have the same type/shape
     Return: a New Matrix or None if they cannot be concatonated
     """
+    if axis == 0:
+        return mat1 + mat2
+    elif axis == 1:
+        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
+    else:
+        raise ValueError("Invalid Axis. Try Again")
