e1b493cbdb216690a90e1acfd3ad74d892aa5b56
hashthat <samalberts73@gmail.com>
2024-01-29 00:41:44 -0600

    pycodestyle

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,7 +6,7 @@ adding two matricies by their elements!
 
 def add_matrices2D(mat1, mat2):
     """
-    elements in a zip context of two matricies 
+    elements in a zip context of two matricies
     """
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
@@ -14,7 +14,7 @@ def add_matrices2D(mat1, mat2):
 
     ihat = [[a + b for a, b in zip(row1, row2)]
             for row1, row2 in zip(mat1, mat2)]
-    
+
     # Add the Elements to shape the matricies
 
     return ihat
---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+concatonating two arrays
+"""
+
+def cat_arrays(arr1, arr2):
+    """
+    a function that returns a new concatonated list
+    """
