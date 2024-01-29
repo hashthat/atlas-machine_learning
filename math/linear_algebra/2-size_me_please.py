83ec1f2834e84d953db8349fc6cc6eac6bf7c002
hashthat <samalberts73@gmail.com>
2024-01-28 20:25:49 -0600

    check if the matrix is empty

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
+    # check if the matrix is empty!
+
+    if not matrix or not matrix[0]:
+        return (0,0)
+
+    # find the rows and columns of the matrix for the expression needed to solve the equation.
     rows = len(matrix)
     cols = len(matrix[0]) if matrix and matrix[0] else 0
     shape = [rows, cols] # variable name of rows and cols
