a2dcbbb7963f4106cb84df568981a468c8a6c079
hashthat <samalberts73@gmail.com>
2024-01-28 22:00:55 -0600

    checked

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 """
-matrix_transpose - J and I hat are the base of the vectors for the matrix position
-Returns: Transposed Matrix
+matrix_transpose - J and I hat are the base vectors
+Returns: Transposed Linear Matrix
 
 """
 
@@ -11,4 +11,3 @@ def matrix_transpose(matrix):
 
     transposed = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
     return transposed
-
