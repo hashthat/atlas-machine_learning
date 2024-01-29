ba5a4230086d7c287700a9e0eab8e758ee8c238c
hashthat <samalberts73@gmail.com>
2024-01-29 03:38:04 -0600

    flabbergasting

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+calculating the shape of a numpy.ndarray
+"""
+
+
+def np_shape(matrix):
+    """
+    - no loops
+    - no try/except statements
+    - Return: Shape as Tuple
+    """
+    return matrix.shape
