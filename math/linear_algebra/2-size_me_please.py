c56afb972a1db6890fdb165af0c591498ec47a2a
hashthat <samalberts73@gmail.com>
2024-01-28 20:17:52 -0600

    tupleS

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,6 +3,6 @@
 def matrix_shape(matrix):
     rows = len(matrix)
     cols = len(matrix[0]) if matrix and matrix[0] else 0
-    shape = []
-    shape.append(rows, cols)
-    return shape
+    shape = [rows, cols]
+    
+    return tuple(shape)
