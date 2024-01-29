5cb5a21765e9b5d512b18d018606bfac3e61ba9a
hashthat <samalberts73@gmail.com>
2024-01-29 00:52:34 -0600

    gettinBetter

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+concatonate two matricies along a specific axis
+"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    assume all elements with same dimension have the same type/shape
+    Return: a New Matrix or None if they cannot be concatonated
+    """
