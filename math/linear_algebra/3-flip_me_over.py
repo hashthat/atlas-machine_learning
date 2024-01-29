c2144a9576a6c1b2e5746f7882c56d039975ff0f
hashthat <samalberts73@gmail.com>
2024-01-28 21:45:58 -0600

    indentation

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -15,6 +15,6 @@ def matrix_transpose(matrix):
 
     """
     transposed_matrix = [[row[i] for row in matrix]
-            for i in range(len(matrix[0]))]
+                for i in range(len(matrix[0]))]
 
     return transposed_matrix
