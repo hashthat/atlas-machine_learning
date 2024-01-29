ac293083adc651f57df3ed27b5f754cebc4cbfbb
hashthat <samalberts73@gmail.com>
2024-01-29 00:36:39 -0600

    zipping similar elements given the matricies being concatonated and observed!

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,6 +6,15 @@ adding two matricies by their elements!
 
 def add_matrices2D(mat1, mat2):
     """
-    elements of a matrix being added together when possible
+    elements in a zip context of two matricies 
     """
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    # choosing ihat or jhat as the basis of the equation!
 
+    ihat = [[a + b for a, b in zip(row1, row2)]
+            for row1, row2 in zip(mat1, mat2)]
+    
+    # Add the Elements to shape the matricies
+
+    return ihat
