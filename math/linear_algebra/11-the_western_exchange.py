0cee0d9b9213c33d998ec291ff046099d21c5db4
hashthat <samalberts73@gmail.com>
2024-01-29 04:04:21 -0600

    syntax error given the transposition function of numpy

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -6,11 +6,10 @@ utilizing numpy.ndarray as a tool of transformation
 
 def np_transpose(matrix):
     """
-    returns the new matricies
+    returns matrix.transpose()
     more importnantly the function
     or numpy import tranposes data
-    of a matricies through the
-    operation or the shaping through
-    tuples
+    of a the given matrix through the
+    operation through the shaping tuple
     """
-    return np.transpose(matrix)  # https://bit.ly/np_transpose
+    return matrix.transpose()  # https://bit.ly/np_transpose
