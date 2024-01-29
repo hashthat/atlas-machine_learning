a1fd92d3b218f08f4b4f00c29912d23bd6af73bd
hashthat <samalberts73@gmail.com>
2024-01-29 03:56:49 -0600

    numpy references for documenting the mathematical functions of pycodestyle and science!

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+utilizing numpy.ndarray as a tool of transformation
+"""
+
+
+def np_transpose(matrix):
+    """
+    returns the new matricies
+    more importnantly the function
+    or numpy import tranposes data
+    of a matricies through the
+    operation or the shaping through
+    tuples
+    """
+    return np.transpose(matrix)  # https://bit.ly/np_transpose
