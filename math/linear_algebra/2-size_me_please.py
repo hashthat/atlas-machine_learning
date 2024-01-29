2bef6ffc6d8851e3b3ee1a1aae4e58ee8ad1772d
hashthat <samalberts73@gmail.com>
2024-01-28 20:07:29 -0600

    shapes

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-# finding the rows and columns of a matricies through len
 
 def matrix_shape(matrix):
     rows = len(matrix)
     cols = len(matrix[0]) if matrix and matrix[0] else 0
-    return rows, cols
+    shape = []
+    shape.append(rows, cols)
+    return shape
