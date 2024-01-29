2a7ba5507b1d36f04ec6e06ac580d540b1fa7fa2
hashthat <samalberts73@gmail.com>
2024-01-29 01:18:30 -0600

    correctingErrorsPycodestyle

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -10,8 +10,12 @@ def cat_matrices2D(mat1, mat2, axis=0):
     Return: a New Matrix or None if they cannot be concatonated
     """
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         return mat1 + mat2
     elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
         return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
     else:
-        return None
+        raise ValueError("this is not a Matricies.")
