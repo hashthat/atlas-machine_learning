44665a1444daa506741ba02cbd9b24db8eb83e54
hashthat <samalberts73@gmail.com>
2024-01-29 04:19:44 -0600

    https://bit.ly/np_arithmetic

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+utilizing numpy.ndarray hence mat1 and mat2.
+Return a tuple containing the element-wise sum,
+difference, product and qutient.
+mat1 and mat2 are never empty
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    this function performs elementwise
+    addition, subtraction, multiplication, division
+    """
+
+    if mat1.shape != mat2.shape:
+        raise ValueError("We Do not Coexist.") # https://bit.ly/np_arithmetic
+
+    if 
+    return 
