944b9227f7f17179f1626e98636be9a70f97b73f
hashthat <samalberts73@gmail.com>
2024-01-28 23:21:45 -0600

    adding parameters

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -8,3 +8,8 @@ def add_arrays(arr1, arr2):
     """
     this is just adding two lists together
     """
+    if len(arr1) != len(arr2):
+        return None
+
+    cat = [a + b for a, b in zip(arr1, arr2)]
+    return cat
