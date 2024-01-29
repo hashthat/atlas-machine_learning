83b0d4a4d4b51f67e61864d8922344eaa4937e99
hashthat <samalberts73@gmail.com>
2024-01-28 20:23:03 -0600

    edits

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,6 +3,9 @@
 def matrix_shape(matrix):
     rows = len(matrix)
     cols = len(matrix[0]) if matrix and matrix[0] else 0
-    shape = [rows, cols]
+    shape = [rows, cols] # variable name of rows and cols
+
+    # return the shape with a tuple
+    # error for getting the proper length of the matrix
     
     return tuple(shape)
