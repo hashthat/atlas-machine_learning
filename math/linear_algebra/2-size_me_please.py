108225825bb116d8033814283ae586a4d34953b7
hashthat <samalberts73@gmail.com>
2024-01-28 22:49:56 -0600

    psych

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -6,23 +6,8 @@ the function will express the shape
 
 
 def matrix_shape(matrix):
-    """
-    matrix_shape(matrix) distinguishes an integer or list
-    in which the matrix is printed in
-    
-    if isinstance(matrix, list):
-    """
-        rows = len(matrix)
-        cols = len(matrix[0]) if matrix and isinstance(matrix[0], list) else 1
-        shape = [rows, cols]
-        return tuple(shape)
-    """
-        shape.append(rows)
-        if isinstance(matrix[0], list):
-            shape.append(matrix_shape(matrix[0])[0])
-        elif type(matrix[0]) is int:
-            shape.append(len(matrix))
-        # return the list and int shape of the matrix
-        return shape
-    """
 
+    rows = len(matrix)
+    cols = len(matrix[0]) if matrix and isinstance(matrix[0], list) else 1
+    shape = [rows, cols]
+    return tuple(shape)
