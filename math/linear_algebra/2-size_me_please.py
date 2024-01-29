e7225ec4fbeef81cc9cf70c1512926324a805d52
hashthat <samalberts73@gmail.com>
2024-01-28 20:43:20 -0600

    recursion is needed

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
-    # check if the matrix is empty!
     
-    if not matrix or not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        riase ValueError("the input recieved is not a valid matrix, this is your valid TypeError warning")
-
+    # is the number a list or an integer?? I need some Recursive help!!!
     # find the rows and columns of the matrix for the expression needed to solve the equation.
-    rows = len(matrix)
-    cols = len(matrix[0]) if matrix and matrix[0] else 0
-    # shape = [rows, cols] # variable name of rows and cols
+        
+        rows = len(matrix)
+        cols = len(matrix[0]) if matrix and matrix[0] else 0
+        shape = []
+    
+        shape.append(rows)
+        shape.append(cols)
 
     # return the shape with a tuple
     # error for getting the proper length of the matrix
     
-    return rows, cols
+        return shape
