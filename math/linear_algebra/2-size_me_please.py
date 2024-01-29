4ade45e715fd02cb42e97b4f8c0ca3e64c48d9d2
hashthat <samalberts73@gmail.com>
2024-01-28 20:55:40 -0600

    appending

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,17 +3,17 @@
 def matrix_shape(matrix):
     
     # check if the matrix is a list or integer
-
+    if isinstance(matrix, list)
     # is the number a list or an integer?? I need some Recursive help!!!
     # find the rows and columns of the matrix for the expression needed to solve the equation.
         
         rows = len(matrix)
-        cols = len(matrix[0]) if matrix and matrix[0] else 0
+        # cols = len(matrix[0]) if matrix and matrix[0] else 0
         shape = []
         
         
         shape.append(rows)
-        shape.append(cols)
+        # shape.append(cols)
 
     # return the shape with a tuple
     # error for getting the proper length of the matrix
