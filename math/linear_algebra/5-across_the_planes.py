14bcd2aa55ce4889b03d87973c991b3ac4a6f080
hashthat <samalberts73@gmail.com>
2024-01-28 23:32:43 -0600

    format

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+adding two matricies by their elements!
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    elements of a matrix being added together when possible
+    """
+
