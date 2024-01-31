558fac94c604a625f64d930d38eb6bf297b14f76
hashthat <samalberts73@gmail.com>
2024-01-31 02:46:06 -0600

    updating spelling for results

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,6 +5,7 @@ n is the limiting factor a.k.a. the stopping condition.
 Return: None if n is not a valid number.
 """
 
+
 def summation_i_squared(n):
     """
     the summation of the equation utilizes the addition of the
@@ -13,11 +14,8 @@ def summation_i_squared(n):
     """
     if not isinstance(n, int) or n <= 0:
         return None
-    
 
     if n == 1:
         return 1
 
-
-    return n**2 + sumation_i_squared(n - 1)
-
+    return n**2 + summation_i_squared(n - 1)
