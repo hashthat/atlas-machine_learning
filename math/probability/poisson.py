023fd96415d141eac73415b92c1fb3c3d377da40
hashthat <samalberts73@gmail.com>
2024-02-02 01:02:14 -0600

    indentation

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -12,19 +12,19 @@ class Poisson:
     """
 
 
-def __init__(self, data=None, lambtha=1.0):
-    """
-    creating the prediction through an intuitive series
-    """
-    if data is None:
-        if lambtha <= 0:
-            raise ValueError("lambtha must be a positive value")
-        self.lambtha = float(lambtha)
-        self.data = None
-    else:
-        if not isinstance(data, list):
-            raise TypeError("data must be a list")
-        if len(data) < 2:
-            raise ValueError("data must contain multiple values")
-        self.lambtha = sum(data) / len(data)
-        self.data = data
+    def __init__(self, data=None, lambtha=1.0):
+        """
+        creating the prediction through an intuitive series
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+            self.data = None
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = sum(data) / len(data)
+            self.data = data
