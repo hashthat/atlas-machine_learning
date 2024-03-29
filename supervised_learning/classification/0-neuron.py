a5a7c55d8b703adbc9176f808301692545f85bdf
hashthat <samalberts73@gmail.com>
2024-03-29 15:18:34 -0500

    updating

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+import numpy as np
+'''
+creating a neuron class
+'''
+
+
+class Neuron:
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
