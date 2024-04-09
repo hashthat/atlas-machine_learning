0e8f41a0db22a9fde9ce6b5991648c930d51616f
hashthat <samalberts73@gmail.com>
2024-04-08 22:04:44 -0500

    getters

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+""" creating a neural network from scratch!!"""
 import numpy as np
 
 
@@ -12,48 +12,41 @@ class NeuralNetwork():
     """
 
     def __init__(self, nx, nodes):
-        # NX < 1
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
-        if type(nx) is int and nx < 1:
+        if nx <= 0:
             raise ValueError("nx must be a positive integer")
-
-        # Validate nodes
         if type(nodes) is not int:
             raise TypeError("nodes must be an integer")
-        if type(nodes) is int and nodes < 1:
+        if nodes <= 0:
             raise ValueError("nodes must be a positive integer")
-
-        # Initialize weights and biases for the hidden layer
         self.__W1 = np.random.randn(nodes, nx)
         self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
-
-        # Initialize weights and biases for the output neuron
         self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
         self.__A2 = 0
 
-        @property
-        def W1(self):
-            return self.__W1
+    @property
+    def W1(self):
+        return self.__W1
 
-        @property
-        def b1(self):
-            return self.__b1
+    @property
+    def b1(self):
+        return self.__b1
 
-        @property
-        def A1(self):
-            return self.__A1
+    @property
+    def A1(self):
+        return self.__A1
 
-        @property 
-        def W2(self):
-            return self.__W2
+    @property
+    def W2(self):
+        return self.__W2
 
-        @property
-        def b2(self):
-            return self.__b2
+    @property
+    def b2(self):
+        return self.__b2
 
-        @property
-        def A2(self):
-            return self.__A2
+    @property
+    def A2(self):
+        return self.__A2
